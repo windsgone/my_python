c51806ef589068fa7908582edca6c4291807f6e9
windsgone <fzywind@gmail.com>
2013-04-24 13:47:17 +0800

    1.1

---
+++ excercise/PygLatin.py
@@ -0,0 +1,17 @@
+original = raw_input('Enter a word:')
+word = original.lower()
+first = word[0]
+new_word = word + pyg
+pyg = 'ay'
+
+"""判断输入的单词是否可用，若可用按首字母类型进行处理
+"""
+
+if len(original) > 0 and original.isalpha():
+    if first == "a" or first == "e" or first == "i" or first == "o" or first == "u":
+        print new_word
+    else:
+        new_word = word[1:] + first + pyg
+        print new_word
+else:
+    print 'empty'
