6961974b88076c4b1179c98d6158a912d0a1edf8
windsgone <fzywind@gmail.com>
2013-05-09 00:36:00 +0800

    05090035

---
+++ lpthw/ex40.py
@@ -0,0 +1,22 @@
+cities = {'CA': 'San Francisco', 'MI': 'Detroit', 'FL': 'Jacksonville'}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+def find_city(themap, state):
+	if state in themap:
+		return themap[state]
+	else:
+		return "Not found."
+
+cities['_find'] = find_city
+
+while True:
+	print "State? (ENTER to quit)",
+	state = raw_input("> ")
+
+	if not state: break
+
+	city_found = cities['_find'](cities, state)
+	# city_found = find_city(cities, state)
+	print city_found
